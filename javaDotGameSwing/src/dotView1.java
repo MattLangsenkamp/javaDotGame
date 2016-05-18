b3fb13735917df80872d24e813d7582a127ee7f4
MattLangsenkamp <mattlangsenkamp@yahoo.com>
2016-05-18 13:06:52 -0400

    added classes to implement interfaces

---
+++ javaDotGameSwing/src/dotView1.java
@@ -0,0 +1,49 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+
+public class dotView1 implements dotView {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerObserver(dotController controll) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatScorePlayerOneScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatePlayerTwoScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTurn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateClickAllowed(JButton clicked) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
